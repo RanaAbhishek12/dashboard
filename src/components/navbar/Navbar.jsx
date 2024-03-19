1aea7e08ed5eaf828d5e4c9bf80e188f3145ec3f
Rana Abhishek Singh <146855051+RanaAbhishek12@users.noreply.github.com>
2024-03-19 16:18:17 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Image src="https://github.com/RanaAbhishek12/dashboard/blob/76955ad91ab2138168ba78841162a6a44ab461cb/almalogo%20(1).png" />
+        <Image src="src/components/navbar/Navbar.jsx />
       </Wrapper>
     </Container>
   );
